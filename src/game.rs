fc07df10b37310fd5489eeb106974f0d34a4b02d
Maria <msanmiguelsuarez@gmail.com>
2024-08-14 00:54:56 +0200

    Initial commit.

---
+++ src/game.rs
@@ -0,0 +1,1 @@
+pub mod bimatrix_game;
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+//use ndarray::prelude::*;
+use ndarray::Array;
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+
+pub mod game;
+//use game::bimatrix_game;
+
+fn main() {
+    let a = Array::random((30, 40), Uniform::new(0., 10.));
+    let b = Array::random((30, 40), Uniform::new(0., 10.));
+    //let a = array![[3., 5., 6.], [6., 1., 5.],];
+    //let b = array![[4., 2., 3.], [2., 4., 1.],];
+
+    let game = game::bimatrix_game::BimatrixGame::new(a, b);
+
+    let eq = game.lemke_howson();
+
+    println!("{:?}", game.strategy_payoff(&eq));
+    println!("regret: {:?}", game.regret(&eq));
+}
