f09eeedc79943f3f10d61da9c2cf32cce35bcbcf
gauleng <gauleng@gmail.com>
2024-08-21 23:26:09 +0200

    Refactorizado el cálculo del resultado para desglosar el lance del tanteo.

--- src/mus/lance.rs
+++ src/mus/lance.rs
@@ -1,10 +1,22 @@
 use super::MusError;
 use crate::mus::Accion;
-use crate::mus::Juego;
 use crate::mus::Mano;
-use crate::mus::Pares;
 
 use std::cmp;
+
+#[derive(PartialOrd, Ord, PartialEq, Eq)]
+pub enum Juego {
+    Resto(u8),
+    Treintaydos,
+    Treintayuna,
+}
+
+#[derive(PartialOrd, Ord, PartialEq, Eq)]
+pub enum Pares {
+    Pareja(u16),
+    Medias(u16),
+    Duples(u16),
+}
 
 #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
 pub enum Lance {
@@ -15,7 +27,95 @@ pub enum Lance {
     Juego,
 }
 
-impl Lance {
+impl Mano {
+    /// Convierte la mano a un entero de 4 bytes. La cuarta carta se mapea al primer byte, la
+    /// tercera al segundo byte, y así sucesivamente. Este valor permite ordenar las manos en el
+    /// lance de grande.
+    pub fn valor_grande(&self) -> usize {
+        let c = self.cartas();
+        (c[3].valor() as usize) << 24
+            | (c[2].valor() as usize) << 16
+            | (c[1].valor() as usize) << 8
+            | c[0].valor() as usize
+    }
+
+    /// Convierte la mano a un entero de 4 bytes. La primera carta se mapea al primer byte, la
+    /// segunda al segundo byte, y así sucesivamente. Este valor permite ordenar las manos en el
+    /// lance de chica.
+    pub fn valor_chica(&self) -> usize {
+        let c = self.cartas();
+        (c[0].valor() as usize) << 24
+            | (c[1].valor() as usize) << 16
+            | (c[2].valor() as usize) << 8
+            | c[3].valor() as usize
+    }
+
+    /// Devuelve el número de parejas de la mano. Si son pares devuelve 1, si son medias devuelve 2
+    /// y si son duples 3. En caso de que no haya parejas, devuelve 0.
+    pub fn pares(&self) -> Option<Pares> {
+        let mut contadores = [0; 13];
+        let c = self.cartas();
+        c.iter().for_each(|c| contadores[c.valor() as usize] += 1);
+
+        let mut grupos = [0; 5];
+
+        contadores
+            .iter()
+            .enumerate()
+            .for_each(|(valor, num)| grupos[*num] |= 1 << valor);
+
+        if grupos[4] > 0 {
+            return Some(Pares::Duples(grupos[4] << 1));
+        }
+        if grupos[3] > 0 {
+            return Some(Pares::Medias(grupos[3]));
+        }
+        if grupos[2] > 0 {
+            if grupos[2].count_ones() == 2 {
+                return Some(Pares::Duples(grupos[2]));
+            }
+            return Some(Pares::Pareja(grupos[2]));
+        }
+        None
+    }
+
+    /// Devuelve los puntos de la mano para los lances de punto y juego.
+    pub fn valor_puntos(&self) -> u8 {
+        let c = self.cartas();
+        c.iter().fold(0, |acc, c| {
+            if c.valor() >= 10 {
+                acc + 10
+            } else {
+                acc + c.valor()
+            }
+        })
+    }
+
+    pub fn juego(&self) -> Option<Juego> {
+        let p = self.valor_puntos();
+        match p {
+            31 => Some(Juego::Treintayuna),
+            32 => Some(Juego::Treintaydos),
+            33..=40 => Some(Juego::Resto(p)),
+            _ => None,
+        }
+    }
+}
+
+pub trait RankingManos {
+    fn compara_manos(&self, a: &Mano, b: &Mano) -> cmp::Ordering;
+    ///
+    /// Dado un vector de manos de mus, devuelve el índice de la mejor de ellas dado el lance en
+    /// juego. Se asume que la primera mano del vector es la del jugador mano y la última la del
+    /// postre.
+    fn mejor_mano(&self, manos: &[Mano]) -> usize {
+        let mut indices: Vec<usize> = (0..manos.len()).rev().collect();
+        indices.sort_by(|i, j| self.compara_manos(&manos[*i], &manos[*j]));
+        *indices.last().unwrap()
+    }
+}
+
+impl RankingManos for Lance {
     fn compara_manos(&self, a: &Mano, b: &Mano) -> cmp::Ordering {
         match self {
             Lance::Grande => a.valor_grande().cmp(&b.valor_grande()),
@@ -25,16 +125,9 @@ impl Lance {
             Lance::Punto => a.valor_puntos().cmp(&b.valor_puntos()),
         }
     }
+}
 
-    /// Dado un vector de manos de mus, devuelve el índice de la mejor de ellas dado el lance en
-    /// juego. Se asume que la primera mano del vector es la del jugador mano y la última la del
-    /// postre.
-    pub fn mejor_mano(&self, manos: &[Mano]) -> usize {
-        let mut indices: Vec<usize> = (0..manos.len()).rev().collect();
-        indices.sort_by(|i, j| self.compara_manos(&manos[*i], &manos[*j]));
-        *indices.last().unwrap()
-    }
-
+impl Lance {
     pub fn tantos_mano(&self, mano: &Mano) -> u8 {
         match self {
             Lance::Pares => match mano.pares() {
@@ -59,6 +152,7 @@ pub struct EstadoLance {
     turno: Option<usize>,
     ultimo_envite: usize,
     apuesta_maxima: u8,
+    ganador: Option<usize>,
 }
 
 impl EstadoLance {
@@ -69,6 +163,7 @@ impl EstadoLance {
             turno: Some(0),
             ultimo_envite: 0,
             apuesta_maxima,
+            ganador: None,
         }
     }
 
@@ -102,6 +197,9 @@ impl EstadoLance {
             }
         }
         self.turno = self.pasar_turno();
+        if self.turno.is_none() && !self.se_quieren() {
+            self.ganador = if self.activos[0] { Some(0) } else { Some(1) };
+        }
         Ok(self.turno)
     }
 
@@ -127,29 +225,45 @@ impl EstadoLance {
         // }
     }
 
-    pub fn tantos(&self, manos: &[Mano], lance: &Lance) -> Option<Vec<u8>> {
-        if self.turno.is_some() {
-            return None;
-        }
-        let jugadores = [0, 1];
-        let activos: Vec<usize> = jugadores.into_iter().filter(|&a| self.activos[a]).collect();
-        let se_quisieron = activos.len() > 1;
-        let mut apostado = if se_quisieron {
+    fn se_quieren(&self) -> bool {
+        self.turno.is_none() && self.activos.iter().all(|b| *b)
+    }
+
+    fn tantos_apostados(&self) -> u8 {
+        if self.se_quieren() {
             self.bote[1]
         } else {
             self.bote[0]
-        };
+        }
+    }
+
+    pub fn resolver_lance<L>(&mut self, manos: &[Mano], lance: &L) -> Option<usize>
+    where
+        L: RankingManos,
+    {
+        self.ganador = Some(lance.mejor_mano(manos) % 2);
+        self.ganador
+    }
+
+    pub fn ganador(&self) -> Option<usize> {
+        self.ganador
+    }
+
+    pub fn tantos(&mut self, manos: &[Mano], lance: &Lance) -> Option<Vec<u8>> {
+        if self.turno.is_some() {
+            return None;
+        }
+        let mut apostado = self.tantos_apostados();
         if let Lance::Grande | Lance::Chica = lance {
             if apostado == 0 {
                 apostado = 1;
             }
         }
 
-        let ganador = if se_quisieron {
-            activos[lance.mejor_mano(manos) % 2]
-        } else {
-            activos[0]
-        };
+        let ganador = self
+            .ganador()
+            .or_else(|| self.resolver_lance(manos, lance))
+            .unwrap();
         let manos_ganadoras = [ganador, ganador + 2];
 
         let mut tantos = vec![0, 0];
@@ -199,13 +313,51 @@ mod tests {
         let mut partida = EstadoLance::new(40);
         let _ = partida.actuar(Accion::Paso);
         let _ = partida.actuar(Accion::Paso);
-
         assert_eq!(partida.tantos(&manos, &Lance::Grande).unwrap(), vec![1, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Paso);
+        let _ = partida.actuar(Accion::Paso);
         assert_eq!(partida.tantos(&manos, &Lance::Chica).unwrap(), vec![1, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Paso);
+        let _ = partida.actuar(Accion::Paso);
         assert_eq!(partida.tantos(&manos, &Lance::Pares).unwrap(), vec![3, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Paso);
+        let _ = partida.actuar(Accion::Paso);
         assert_eq!(partida.tantos(&manos, &Lance::Juego).unwrap(), vec![0, 2]);
     }
 
+    #[test]
+    fn test_tanteo2() {
+        let manos = vec![
+            Mano::try_from("1234").unwrap(),
+            Mano::try_from("57SS").unwrap(),
+            Mano::try_from("3334").unwrap(),
+            Mano::try_from("257C").unwrap(),
+        ];
+
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Quiero);
+        assert_eq!(partida.tantos(&manos, &Lance::Grande).unwrap(), vec![4, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Quiero);
+        assert_eq!(partida.tantos(&manos, &Lance::Chica).unwrap(), vec![4, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Quiero);
+        assert_eq!(partida.tantos(&manos, &Lance::Pares).unwrap(), vec![7, 0]);
+        let mut partida = EstadoLance::new(40);
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Envido(2));
+        let _ = partida.actuar(Accion::Quiero);
+        assert_eq!(partida.tantos(&manos, &Lance::Juego).unwrap(), vec![0, 6]);
+    }
     use std::cmp::Ordering::*;
 
     #[test]
--- src/mus/mano.rs
+++ src/mus/mano.rs
@@ -4,20 +4,6 @@ use super::Carta;
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Mano(Vec<Carta>);
 
-#[derive(PartialOrd, Ord, PartialEq, Eq)]
-pub enum Juego {
-    Resto(u8),
-    Treintaydos,
-    Treintayuna,
-}
-
-#[derive(PartialOrd, Ord, PartialEq, Eq)]
-pub enum Pares {
-    Pareja(u16),
-    Medias(u16),
-    Duples(u16),
-}
-
 impl Mano {
     // Cards in hand are always sorted by value.
     pub fn new(cartas: Vec<Carta>) -> Self {
@@ -26,75 +12,8 @@ impl Mano {
         m
     }
 
-    /// Convierte la mano a un entero de 4 bytes. La cuarta carta se mapea al primer byte, la
-    /// tercera al segundo byte, y así sucesivamente. Este valor permite ordenar las manos en el
-    /// lance de grande.
-    pub fn valor_grande(&self) -> usize {
-        (self.0[3].valor() as usize) << 24
-            | (self.0[2].valor() as usize) << 16
-            | (self.0[1].valor() as usize) << 8
-            | self.0[0].valor() as usize
-    }
-
-    /// Convierte la mano a un entero de 4 bytes. La primera carta se mapea al primer byte, la
-    /// segunda al segundo byte, y así sucesivamente. Este valor permite ordenar las manos en el
-    /// lance de chica.
-    pub fn valor_chica(&self) -> usize {
-        (self.0[0].valor() as usize) << 24
-            | (self.0[1].valor() as usize) << 16
-            | (self.0[2].valor() as usize) << 8
-            | self.0[3].valor() as usize
-    }
-
-    /// Devuelve el número de parejas de la mano. Si son pares devuelve 1, si son medias devuelve 2
-    /// y si son duples 3. En caso de que no haya parejas, devuelve 0.
-    pub fn pares(&self) -> Option<Pares> {
-        let mut contadores = [0; 13];
-        self.0
-            .iter()
-            .for_each(|c| contadores[c.valor() as usize] += 1);
-
-        let mut grupos = [0; 5];
-
-        contadores
-            .iter()
-            .enumerate()
-            .for_each(|(valor, num)| grupos[*num] |= 1 << valor);
-
-        if grupos[4] > 0 {
-            return Some(Pares::Duples(grupos[4] << 1));
-        }
-        if grupos[3] > 0 {
-            return Some(Pares::Medias(grupos[3]));
-        }
-        if grupos[2] > 0 {
-            if grupos[2].count_ones() == 2 {
-                return Some(Pares::Duples(grupos[2]));
-            }
-            return Some(Pares::Pareja(grupos[2]));
-        }
-        None
-    }
-
-    /// Devuelve los puntos de la mano para los lances de punto y juego.
-    pub fn valor_puntos(&self) -> u8 {
-        self.0.iter().fold(0, |acc, c| {
-            if c.valor() >= 10 {
-                acc + 10
-            } else {
-                acc + c.valor()
-            }
-        })
-    }
-
-    pub fn juego(&self) -> Option<Juego> {
-        let p = self.valor_puntos();
-        match p {
-            31 => Some(Juego::Treintayuna),
-            32 => Some(Juego::Treintaydos),
-            33..=40 => Some(Juego::Resto(p)),
-            _ => None,
-        }
+    pub fn cartas(&self) -> &Vec<Carta> {
+        &self.0
     }
 }
 
