0471423ea36e629a8769e2a87fe947b3a5efcb77
gauleng <gauleng@gmail.com>
2024-09-26 11:27:40 +0200

    Métodos para navegar el árbol de acciones.

--- src/game/action_tree.rs
+++ src/game/action_tree.rs
@@ -6,7 +6,10 @@ pub enum ActionNode<P, A> {
     NonTerminal(P, Vec<(A, ActionNode<P, A>)>),
 }
 
-impl<P, A> ActionNode<P, A> {
+impl<P, A> ActionNode<P, A>
+where
+    A: Eq,
+{
     pub fn new(p: P) -> Self {
         Self::NonTerminal(p, Vec::new())
     }
@@ -24,6 +27,15 @@ impl<P, A> ActionNode<P, A> {
             m.last_mut().map(|v| &mut v.1)
         } else {
             None
+        }
+    }
+
+    pub fn next_node(&self, action: A) -> Option<&ActionNode<P, A>> {
+        match self {
+            ActionNode::Terminal => None,
+            ActionNode::NonTerminal(_, children) => {
+                children.iter().find(|&c| c.0 == action).map(|c| &c.1)
+            }
         }
     }
 }
