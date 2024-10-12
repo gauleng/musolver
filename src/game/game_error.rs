82bd0c7d945c2cc3233f3f46e2f7d2119da307e5
gauleng <gauleng@gmail.com>
2024-10-12 12:13:50 +0200

    Mensaje de error más claro.

--- src/game/game_error.rs
+++ src/game/game_error.rs
@@ -2,6 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum GameError {
-    #[error("Invalid CFR method: {0}")]
+    #[error("Invalid CFR method: {0}. Accepted methods are: cfr, cfr-plus, external-sampling and chance-sampling.")]
     InvalidCfrMethod(String),
 }
