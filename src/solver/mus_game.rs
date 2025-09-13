3b8f924cb8bea49c05d55cf2c0ca17e4b902ec00
Pedro Suárez Casal <gauleng@gmail.com>
2025-09-14 00:20:06 +0200

    Terminada la adaptación de mus_game al nuevo trait Game.

--- src/solver/mus_game.rs
+++ src/solver/mus_game.rs
@@ -1,54 +1,80 @@
-use arrayvec::{ArrayString, ArrayVec};
+use arrayvec::ArrayString;
 use itertools::Itertools;
 
 use crate::{
-    mus::{Accion, Apuesta, Baraja, Carta, Lance, Mano, PartidaMus, Turno},
+    mus::{Accion, Apuesta, Baraja, Lance, Mano, PartidaMus, Turno},
     Game, NodeType,
 };
-
-use super::ManosNormalizadas;
 
 #[derive(Debug, Clone)]
 pub struct MusGame {
     tantos: [u8; 2],
     partida: Option<PartidaMus>,
-    history_str: ArrayVec<ArrayString<4>, 14>,
-    info_set_prefix: [ArrayString<64>; 4],
+    history_str: ArrayString<64>,
+    info_set_prefix: [ArrayString<16>; 4],
     last_action: Option<Accion>,
+    manos_pares: ArrayString<4>,
+    manos_juego: ArrayString<4>,
     abstract_game: bool,
 }
 
 impl MusGame {
     pub fn new(tantos: [u8; 2], abstract_game: bool) -> Self {
+        if abstract_game {
+            todo!("Abtract game not supported.")
+        }
         Self {
             partida: None,
             tantos,
-            history_str: ArrayVec::new(),
-            info_set_prefix: [ArrayString::<64>::new(); 4],
+            history_str: ArrayString::new(),
+            info_set_prefix: [ArrayString::new(); 4],
             last_action: None,
+            manos_pares: ArrayString::new(),
+            manos_juego: ArrayString::new(),
             abstract_game,
         }
     }
 
     fn from_partida_mus(partida: PartidaMus, abstract_game: bool) -> Self {
+        if abstract_game {
+            todo!("Abtract game not supported.")
+        }
         let tantos = *partida.tantos();
         Self {
             partida: Some(partida),
             tantos,
-            history_str: ArrayVec::new(),
-            info_set_prefix: [ArrayString::<64>::new(); 4],
+            history_str: ArrayString::new(),
+            info_set_prefix: [ArrayString::new(); 4],
             last_action: None,
+            manos_pares: ArrayString::new(),
+            manos_juego: ArrayString::new(),
             abstract_game,
         }
+    }
+
+    fn jugadas_manos(manos: &[Mano; 4]) -> (ArrayString<4>, ArrayString<4>) {
+        let manos_pares = manos
+            .iter()
+            .map(|m| if m.pares().is_some() { '1' } else { '0' })
+            .join("");
+        let manos_juego = manos
+            .iter()
+            .map(|m| if m.pares().is_some() { '1' } else { '0' })
+            .join("");
+
+        (
+            ArrayString::from(&manos_pares).unwrap(),
+            ArrayString::from(&manos_juego).unwrap(),
+        )
     }
 
     fn info_set_prefix(
         manos: &[Mano; 4],
         tantos: &[u8; 2],
         abstracto: bool,
-    ) -> [ArrayString<64>; 4] {
-        let info_set_prefix: [ArrayString<64>; 4] = core::array::from_fn(|i| {
-            ArrayString::<64>::from(&format!("{}:{},{},", tantos[0], tantos[1], manos[i])).unwrap()
+    ) -> [ArrayString<16>; 4] {
+        let info_set_prefix: [ArrayString<16>; 4] = core::array::from_fn(|i| {
+            ArrayString::from(&format!("{}:{},{},", tantos[0], tantos[1], manos[i])).unwrap()
         });
         info_set_prefix
     }
@@ -66,18 +92,13 @@ impl Game for MusGame {
             tantos[1] as i8 - tantos[0] as i8,
         ];
 
-        payoff[player] as f64
+        payoff[player % 2] as f64
     }
 
     fn info_set_str(&self, player: usize) -> String {
-        let mut output = String::with_capacity(15 + self.history_str.len() + 1);
-        output.push_str(&self.tantos[0].to_string());
-        output.push(':');
-        output.push_str(&self.tantos[1].to_string());
-        output.push(',');
-        for i in self.history_str.iter() {
-            output.push_str(&i.to_string());
-        }
+        let mut output = String::with_capacity(15 + self.history_str.len());
+        output.push_str(&self.info_set_prefix[player]);
+        output.push_str(&self.history_str());
         output
     }
 
@@ -86,6 +107,7 @@ impl Game for MusGame {
         baraja.barajar();
         let manos = baraja.repartir_manos();
         self.info_set_prefix = MusGame::info_set_prefix(&manos, &self.tantos, self.abstract_game);
+        (self.manos_pares, self.manos_juego) = MusGame::jugadas_manos(&manos);
         let partida = PartidaMus::new(manos, self.tantos);
         self.partida = Some(partida);
     }
@@ -109,16 +131,16 @@ impl Game for MusGame {
             Apuesta::Tantos(0) => vec![
                 Accion::Paso,
                 Accion::Envido(2),
-                Accion::Envido(5),
-                Accion::Envido(10),
+                //Accion::Envido(5),
+                //Accion::Envido(10),
                 Accion::Ordago,
             ],
             Apuesta::Tantos(2) => vec![
                 Accion::Paso,
                 Accion::Quiero,
                 Accion::Envido(2),
-                Accion::Envido(5),
-                Accion::Envido(10),
+                //Accion::Envido(5),
+                //Accion::Envido(10),
                 Accion::Ordago,
             ],
             Apuesta::Tantos(4..=5) => vec![
@@ -153,25 +175,42 @@ impl Game for MusGame {
         let turno = self
             .partida
             .as_ref()
-            .expect("At least one EstadoLance must be available.")
+            .expect("At least one PartidaMus must be available.")
             .turno()
-            .unwrap();
+            .expect("One active player must be available.");
         match turno {
-            Turno::Pareja(2) | Turno::Pareja(3) => {
-                self.history_str.pop();
-            }
-            _ => {}
-        };
-        let action_str = match turno {
-            Turno::Pareja(0) | Turno::Pareja(1) => a.to_string() + "*",
-            _ => a.to_string(),
-        };
-        self.history_str
-            .push(ArrayString::<4>::from(&action_str).unwrap());
+            Turno::Pareja(0 | 1) => {}
+            _ => self.history_str.push_str(&a.to_string()),
+        }
+
+        let lance_previo = self.partida.as_mut().unwrap().lance_actual();
         let _ = self.partida.as_mut().unwrap().actuar(a);
+        let lance_siguiente = self.partida.as_mut().unwrap().lance_actual();
+        if lance_previo != lance_siguiente {
+            match lance_siguiente {
+                Some(Lance::Pares) => self.history_str.push_str(self.manos_pares.as_str()),
+                Some(Lance::Juego) => {
+                    if lance_previo != Some(Lance::Pares) {
+                        self.history_str.push_str(self.manos_pares.as_str());
+                    }
+                    self.history_str.push_str(self.manos_juego.as_str());
+                }
+                _ => {}
+            }
+        }
     }
 
     fn history_str(&self) -> String {
-        todo!()
+        let turno = self
+            .partida
+            .as_ref()
+            .expect("At least one EstadoLance must be available.")
+            .turno();
+        match turno {
+            Some(Turno::Pareja(2 | 3)) => {
+                format!("{}{}*", self.history_str, self.last_action.unwrap())
+            }
+            _ => self.history_str.to_string(),
+        }
     }
 }
